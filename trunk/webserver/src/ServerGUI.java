981b685d5fb2089907ba196d2ba0111b8e600819
vlad.lep <vlad.lep@d8a219af-1a4d-31b5-c5cf-55fcf9aeac0a>
2010-12-11 12:53:32 +0000

    - make browse buttons work - validate start stop actions and mentenace entry

--- trunk/webserver/src/ServerGUI.java
+++ trunk/webserver/src/ServerGUI.java
@@ -1,33 +1,34 @@
 import java.awt.BorderLayout;
-import java.awt.Component;
 import java.awt.Container;
-import java.awt.Dimension;
 import java.awt.GridLayout;
-import java.awt.Insets;
 import java.awt.LayoutManager;
 import java.awt.event.ActionEvent;
-import java.text.ParseException;
+import java.awt.event.ActionListener;
+import java.awt.event.ContainerEvent;
+import java.awt.event.ContainerListener;
+import java.awt.event.InputMethodEvent;
+import java.awt.event.InputMethodListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-import javax.swing.InputVerifier;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFileChooser;
 import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.SwingConstants;
-import javax.swing.border.Border;
-import javax.swing.filechooser.FileSystemView;
-import javax.swing.plaf.FileChooserUI;
 import javax.swing.text.MaskFormatter;
 
 public class ServerGUI {
 	public static boolean RIGHT_TO_LEFT = false;
 	private JFrame frame;
+	private static JTextField rootDir;
+	private static JTextField mentenanceDir;
 	
-	public  void addComponentsToPane(Container contentPane) {
+	public void addComponentsToPane(Container contentPane) {
 		// Use BorderLayout. Default empty constructor with no horizontal and
 		// vertical
 		// gaps
@@ -43,93 +44,174 @@ public class ServerGUI {
 					.setComponentOrientation(java.awt.ComponentOrientation.RIGHT_TO_LEFT);
 		}
 
-		
-//		contentPane.add(actionButton, BorderLayout.PAGE_START);
-//
-//		actionButton = new JButton("Button 2 (CENTER)");
-////		actionButton.setPreferredSize(new Dimension(200, 100));
-//		contentPane.add(actionButton, BorderLayout.CENTER);
-//		
-//		actionButton = new JButton("Button 3 (LINE_START)");
-//		contentPane.add(actionButton, BorderLayout.LINE_START);
-//
-//		actionButton = new JButton("Long-Named Button 4 (PAGE_END)");
-//		contentPane.add(actionButton, BorderLayout.PAGE_END);
-//
-		
-		
-		JComponent labelsPanel = new  JPanel();
-		GridLayout labelsLayout =new GridLayout(3,2);
+		// contentPane.add(actionButton, BorderLayout.PAGE_START);
+		//
+		// actionButton = new JButton("Button 2 (CENTER)");
+		// // actionButton.setPreferredSize(new Dimension(200, 100));
+		// contentPane.add(actionButton, BorderLayout.CENTER);
+		//		
+		// actionButton = new JButton("Button 3 (LINE_START)");
+		// contentPane.add(actionButton, BorderLayout.LINE_START);
+		//
+		// actionButton = new JButton("Long-Named Button 4 (PAGE_END)");
+		// contentPane.add(actionButton, BorderLayout.PAGE_END);
+		//
+
+		JComponent labelsPanel = new JPanel();
+		GridLayout labelsLayout = new GridLayout(3, 2);
 		labelsLayout.setHgap(2);
 		labelsLayout.setVgap(2);
 		labelsPanel.setLayout(labelsLayout);
-		
+
 		contentPane.add(labelsPanel, BorderLayout.LINE_START);
-		
+
 		labelsPanel.add(new JLabel("Server status :"));
 		JLabel statusServerLable = new JLabel("Stopped");
 		labelsPanel.add(statusServerLable);
-				
-		labelsPanel.add(new JLabel("Server address :"));
-		JFormattedTextField serverAddress = new JFormattedTextField(createFormatter("###.###.###.###"));
+
+		labelsPanel.add(new JLabel("Server address (*):"));
+		final JFormattedTextField serverAddress = new JFormattedTextField(
+				createFormatter("###.###.###.###"));
 		labelsPanel.add(serverAddress);
-		
-		labelsPanel.add(new JLabel("Server port :"));
-		JFormattedTextField portServer = new JFormattedTextField(createFormatter("#####"));
+
+		labelsPanel.add(new JLabel("Server port (*):"));
+		final JFormattedTextField portServer = new JFormattedTextField(
+				createFormatter("#####"));
 		labelsPanel.add(portServer);
-//		portServer.setText("11");
-//		portServer.setEditable(false);
-		
-		JButton actionButton = new JButton("Start Server");
-		contentPane.add(actionButton, BorderLayout.LINE_END);
-		
+		// portServer.setText("11");
+		// portServer.setEditable(false);
+
+		final JButton actionButton = new JButton("Start Server");
+		contentPane.add(actionButton, BorderLayout.CENTER);
+
+		final JButton mentenaceButton = new JButton("Start Mentenance");
+		contentPane.add(mentenaceButton, BorderLayout.LINE_END);
+
 		JPanel pathPanel = new JPanel();
-		
+
 		contentPane.add(pathPanel, BorderLayout.PAGE_END);
-		LayoutManager pathPanelLayout = new GridLayout(2,3);
+		LayoutManager pathPanelLayout = new GridLayout(2, 3);
 		pathPanel.setLayout(pathPanelLayout);
+
+		pathPanel.add(new JLabel("Root dir (*) : "));
+		rootDir = new JTextField("Insert root dir");
 		
-		pathPanel.add(new JLabel("Root dir : "));
-		JTextField rootDir = new JTextField("Insert root dir");
+		
+		rootDir.addKeyListener(new KeyListener() {
+
+			public void keyTyped(KeyEvent e) {
+				// TODO Auto-generated method stub
+				System.out.println("typed");
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				System.out.println("released");
+
+			}
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+				System.out.println("presed");
+
+			}
+		});
+		
 		pathPanel.add(rootDir);
+
+		final JButton browseButton = new JButton("Browse");
 		
-		JButton browseButton = new JButton("Browse");
 		pathPanel.add(browseButton);
+
+		final JFileChooser browseRoot = new JFileChooser();
 		
-		final  JFileChooser browseRoot = new JFileChooser();
 		browseRoot.changeToParentDirectory();
 		browseRoot.setMultiSelectionEnabled(false);
 		browseRoot.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-		
-		browseButton.addActionListener( new BrowseListener(frame,browseRoot));
-		
+
+		browseButton.addActionListener(new BrowseListener(frame, browseRoot,"Root"));
+
 		pathPanel.add(new JLabel("Mentenance dir : "));
-		JTextField mentenanceDir = new JTextField("Insert mentenance dir");
+		mentenanceDir = new JTextField("Insert mentenance dir");
+		
 		pathPanel.add(mentenanceDir);
-		
-		JButton browseMentButton = new JButton("Browse");
+
+		final JButton browseMentButton = new JButton("Browse");
 		pathPanel.add(browseMentButton);
-		browseMentButton.addActionListener( new BrowseListener(frame,browseRoot));
-		
-		
-		
-	
-		
-		//pathPanel.add(browseRoot);
-		
-		
+		browseMentButton
+				.addActionListener(new BrowseListener(frame, browseRoot,"Mentenace"));
+
+		actionButton.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+
+				if (actionButton.getText().equals("Start Server")) {
+					System.out.println(portServer.getText().trim()+"\"") ;
+					if(serverAddress.getText().equals("   .   .   .   ") || portServer.getText().trim()=="" )
+					{
+						JOptionPane.showMessageDialog(frame,"Elements marked as (*) can not be empty");
+						return;
+					}
+					serverAddress.setEditable(false);
+					portServer.setEditable(false);
+					rootDir.setEditable(false);
+					browseButton.setEnabled(false);
+					actionButton.setText("Stop Server");
+				} else {
+					//stop server action
+					serverAddress.setEditable(true);
+					portServer.setEditable(true);
+					rootDir.setEditable(true);
+					browseButton.setEnabled(true);
+					actionButton.setText("Start Server");
+					browseMentButton.setEnabled(true);
+					mentenaceButton.setText("Start Mentenance");
+					mentenanceDir.setEditable(true);
+				}
+			}
+		});
+
+		mentenaceButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				if (mentenaceButton.getText().equals("Start Mentenance")) {
+					if(actionButton.getText().equals("Start Server"))
+					{
+						JOptionPane.showMessageDialog(frame,"Start server first");
+						return;
+					}
+					rootDir.setEditable(true);
+					browseButton.setEnabled(true);
+					browseMentButton.setEnabled(false);
+					mentenaceButton.setText("Stop Mentenace");
+					mentenanceDir.setEditable(false);
+				}
+				else
+				{
+					browseMentButton.setEnabled(true);
+					mentenaceButton.setText("Start Mentenance");
+					mentenanceDir.setEditable(true);
+					if(actionButton.getText().equals("Stop Server"))
+					{
+						rootDir.setEditable(false);
+						browseButton.setEnabled(false);
+					}
+				}
+			}
+		});
+
 	}
 
-	
 	protected static MaskFormatter createFormatter(String s) {
-	    MaskFormatter formatter = null;
-	    try {
-	        formatter = new MaskFormatter(s);
-	    } catch (java.text.ParseException exc) {
-	        System.err.println("formatter is bad: " + exc.getMessage());
-	        System.exit(-1);
-	    }
-	    return formatter;
+		MaskFormatter formatter = null;
+		try {
+			formatter = new MaskFormatter(s);
+		} catch (java.text.ParseException exc) {
+			System.err.println("formatter is bad: " + exc.getMessage());
+			System.exit(-1);
+		}
+		return formatter;
 	}
 
 	private void createAndShowGUI() {
@@ -152,4 +234,14 @@ public class ServerGUI {
 			}
 		});
 	}
+	public static void setRootDir(String newRoot)
+	{
+		rootDir.setText(newRoot);
+	}
+	
+	public static void setMentDir(String newDir)
+	{
+		mentenanceDir.setText(newDir);
+	}
+	
 }
