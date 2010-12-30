210345b8131e98a4fe7f194ed2ea72969fc9afe9
vlad.lep <vlad.lep@d8a219af-1a4d-31b5-c5cf-55fcf9aeac0a>
2010-12-30 20:55:05 +0000

    legat GUI de server. probleme : 1.nu se poate reporni serverul dupa ce il opresc; 2. se mai poate primi o cerere dupa ce am oprit serverul;

---
+++ trunk/webserver/src/server/ClientServer.java
@@ -0,0 +1,45 @@
+package server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ClientServer extends Thread {
+	
+	private Socket clientSocket;
+	
+	public ClientServer(Socket clientSoc) {
+		clientSocket = clientSoc;
+		start();
+	}
+
+	public void run() {
+		System.out.println("New Communication Thread Started");
+
+		try {
+			PrintWriter out = new PrintWriter(clientSocket.getOutputStream(),
+					true);
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					clientSocket.getInputStream()));
+
+			String inputLine;
+			
+			while ((inputLine = in.readLine()) != null) {
+				System.out.println("Server: " + inputLine);
+				out.println(inputLine);
+
+				if (inputLine.trim().equals(""))
+					break;
+			}
+
+			out.close();
+			in.close();
+			clientSocket.close();
+		} catch (IOException e) {
+			System.err.println("Problem with Communication Server");
+			System.exit(1);
+		}
+	}
+}
--- trunk/webserver/src/server/WebServer.java
+++ trunk/webserver/src/server/WebServer.java
@@ -4,66 +4,65 @@ import java.net.*;
 import java.io.*;
 
 public class WebServer extends Thread {
-	protected Socket clientSocket;
-
-	public static void main(String[] args) throws IOException {
+	
+	public static boolean serverStarted;
+	public final int port;
+	public final String pathRoot;
+	public final String pathMent;
+	public static WebServer server = null;
+	
+	public void run() {
 		ServerSocket serverSocket = null;
-
+		serverStarted = true;
 		try {
-			serverSocket = new ServerSocket(10008);
+			serverSocket = new ServerSocket(port);
 			System.out.println("Connection Socket Created");
 			try {
-				while (true) {
+				while (serverStarted) {
 					System.out.println("Waiting for Connection");
-					new WebServer(serverSocket.accept());
+					new ClientServer(serverSocket.accept());
 				}
 			} catch (IOException e) {
 				System.err.println("Accept failed.");
 				System.exit(1);
 			}
 		} catch (IOException e) {
-			System.err.println("Could not listen on port: 10008.");
+			System.err.println("Could not listen on port: "+port+".");
 			System.exit(1);
 		} finally {
 			try {
 				serverSocket.close();
 			} catch (IOException e) {
-				System.err.println("Could not close port: 10008.");
+				System.err.println("Could not close port: "+port+".");
 				System.exit(1);
 			}
 		}
 	}
 
-	private WebServer(Socket clientSoc) {
-		clientSocket = clientSoc;
-		start();
+	private WebServer(int port, String pathRoot, String pathMent) {
+	this.port = port;
+	this.pathRoot = pathRoot;
+	this.pathMent = pathMent;
+	
 	}
 
-	public void run() {
-		System.out.println("New Communication Thread Started");
-
-		try {
-			PrintWriter out = new PrintWriter(clientSocket.getOutputStream(),
-					true);
-			BufferedReader in = new BufferedReader(new InputStreamReader(
-					clientSocket.getInputStream()));
-
-			String inputLine;
-			
-			while ((inputLine = in.readLine()) != null) {
-				System.out.println("Server: " + inputLine);
-				out.println(inputLine);
-
-				if (inputLine.trim().equals(""))
-					break;
-			}
-
-			out.close();
-			in.close();
-			clientSocket.close();
-		} catch (IOException e) {
-			System.err.println("Problem with Communication Server");
-			System.exit(1);
-		}
+	public static void startServer(int port, String pathRoot, String pathMent)
+	{
+		if(server == null)
+			server = new WebServer(port, pathRoot, pathMent);
+		System.out.println(server.toString());
+		server.start();
+	}
+	
+	public static void stopServer()
+	{
+		serverStarted = false;
+		
+	}
+	
+	@Override
+	public String toString() {
+	
+	return "port: "+port+" root: "+pathRoot+" ment : "+pathMent;
 	}
 }
