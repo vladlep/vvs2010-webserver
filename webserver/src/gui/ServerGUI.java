1e9a31eb42c315e9041cbcc780615c3a4b2f6c0d
vlad.lep <vlad.lep@d8a219af-1a4d-31b5-c5cf-55fcf9aeac0a>
2010-12-30 22:00:47 +0000

    problema : - nu merge Junit-ul pt eclipseul instalat. - mai multe buguri in firebug

--- webserver/src/gui/ServerGUI.java
+++ webserver/src/gui/ServerGUI.java
@@ -9,7 +9,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.io.File;
-import java.io.IOException;
+
 
 import javax.swing.JButton;
 import javax.swing.JComponent;
---
+++ webserver/src/test/WebServerTest.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.junit.Test;
+
+public class WebServerTest {
+
+	@Test
+	public void startServerTest()
+	{
+		System.out.println();
+		assert(true);
+	}
+}
